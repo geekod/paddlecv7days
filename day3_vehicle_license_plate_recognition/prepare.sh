d6edaddf2ff349ca2a1c2006e2fcc3e8b3ab8f33
geekod <geekod@hotmail.com>
2020-04-03 08:19:35 +0800

    Create prepare.sh

---
+++ day3_vehicle_license_plate_recognition/prepare.sh
@@ -0,0 +1,12 @@
+# 查看当前挂载的数据集目录, 该目录下的变更重启环境后会自动还原
+# View dataset directory. This directory will be recovered automatically after resetting environment. 
+ls /home/aistudio/data
+
+# 查看工作区文件, 该目录下的变更将会持久保存. 请及时清理不必要的文件, 避免加载过慢.
+# View personal work directory. All changes under this directory will be kept even after reset. Please clean unnecessary files in time to speed up environment loading.
+ls /home/aistudio/work
+
+
+rm -rf __MACOSX
+unzip -q /home/aistudio/data/data23617/characterData.zip
+
